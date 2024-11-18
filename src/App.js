c80deb64f9617a95f27e63cb77cd4ad794d5e897
matheusknaul <contatomatheush0909@gmail.com>
2024-11-18 17:10:22 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
@@ -12,15 +11,6 @@ import Footer from './components/layout/Footer';
 import Home from './components/pages/Home';
 import Normas from './components/pages/Normas';
 import AnaliseCritica from './components/pages/AnaliseCritica';
-=======
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import Contato from './pages/Contato';
-import Empresa from './pages/Empresa';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
->>>>>>> f472ec48dbcddf491467b1f782c8815b83818393
 
 function App() {
 
@@ -28,15 +18,12 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Home/>} />
         <Route path="/normas" element={<Normas/>} />
         <Route path="/analisecritica" element={<AnaliseCritica/>} />
-=======
         <Route path="/" element={<Home />}/>
         <Route path="/empresa" element={<Empresa />}/>
         <Route path="/contato" element={<Contato />}/>
->>>>>>> f472ec48dbcddf491467b1f782c8815b83818393
       </Routes>
       <Footer/>
     </Router>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 import styles from './Navbar.module.css'
-<<<<<<< HEAD
+
 import Container from './Container';
 
 function Navbar(){
@@ -13,16 +13,6 @@ function Navbar(){
                     <li className={styles.item}><Link to="/analisecritica">Análise Crítica</Link></li>
                 </ul>
         </nav>
-=======
-
-function Navbar(){
-    return(
-        <ul className={styles.list}>
-            <li className={styles.item}><Link to="/">Home</Link></li>
-            <li className={styles.item}><Link to="/empresa">Empresa</Link></li>
-            <li className={styles.item}><Link to="/contato">Contato</Link></li>
-        </ul>
->>>>>>> f472ec48dbcddf491467b1f782c8815b83818393
     )
 }
 
